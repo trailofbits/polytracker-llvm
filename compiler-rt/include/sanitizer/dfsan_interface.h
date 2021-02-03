4434ce5e8e0e5b40c1dddb4233312c7d563c5fd7
carsonharmon <carson.harmon@trailofbits.com>
2021-02-03 17:58:53 -0500

    Update exported label size in interface to 32

--- compiler-rt/include/sanitizer/dfsan_interface.h
+++ compiler-rt/include/sanitizer/dfsan_interface.h
@@ -21,7 +21,7 @@
 extern "C" {
 #endif
 
-typedef uint16_t dfsan_label;
+typedef uint32_t dfsan_label;
 
 /// Stores information associated with a specific label identifier.  A label
 /// may be a base label created using dfsan_create_label, with associated
